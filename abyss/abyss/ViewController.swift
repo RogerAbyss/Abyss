b1fc933b1a7c71e5f371c1ae523e1596c35958da
abyss <roger_ren@qq.com>
2018-02-27 12:17:13 +0800

    集成Fabric 的Crashlytics，因为崩溃日志原因 不好检查是否成功

--- abyss/abyss/ViewController.swift
+++ abyss/abyss/ViewController.swift
@@ -7,12 +7,26 @@
 //
 
 import UIKit
+import Crashlytics
 
-class ViewController: Weex {
+class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let button = UIButton(type: .roundedRect)
+        button.frame = CGRect(x: 20, y: 50, width: 100, height: 30)
+        button.setTitle("Crash", for: [])
+        button.addTarget(self, action: #selector(self.crashButtonTapped(_:)), for: .touchUpInside)
+        view.addSubview(button)
+
+    }
+
+    @IBAction func crashButtonTapped(_ sender: AnyObject) {
+//        Crashlytics.sharedInstance().crash()
+        let a = [""]
+        let b = a[2]
     }
 
     override func didReceiveMemoryWarning() {
